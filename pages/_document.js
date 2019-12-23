a724d6c5bc7d6e2dcc40bce263cbebc73d962e85
hhoomph <hh.oomph@gmail.com>
2019-12-23 15:52:19 +0330

    change _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,16 +12,15 @@ class MyDocument extends Document {
       <Html lang="fa">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
-          <meta name="apple-mobile-web-app-title" content="Qarun" />
           <meta name="apple-mobile-web-app-status-bar-style" content="light" />
           <link rel="apple-touch-icon" href="../static/img/splash.png" />
           <link rel="apple-touch-startup-image" href="../static/img/splash.png" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="description" content="App created for test development" />
+          <meta name="description" content="قارون" />
           <link rel="manifest" href="../manifest.json" />
           <link rel="shortcut icon" href="../favicon.ico" />
-          <meta name="theme-color" content="#f96d01" />
+          <meta name="theme-color" content="#fff" />
           <meta httpEquiv="ScreenOrientation" content="autoRotate:disabled" />
         </Head>
         <body>
